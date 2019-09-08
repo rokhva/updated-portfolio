648acd06f660cf36406e48b9b62ff077639bcee3
Bahaar Rokhva <rokhvab@gmail.com>
2019-09-07 21:09:07 -0600

    got links working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,15 @@
 
-$('#email').click(function() {
-    console.log("hi");
+
+$(document).ready(function(){
+    $('.slide-section').click(function(e){
+        let linkHref = $(this).attr('href');
+
+        
+        $('html, body').animate({
+            scrollTop: $(linkHref).offset().top
+        }, 1000);
+
+
+        e.preventDefault();
+    });
 });
